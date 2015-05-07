0e69358c6f3e8ef5ebc8945346fbb64bdf7fb2a3
smutt <smutt@depht.com>
2015-05-07 12:17:36 -0700

    Numerous changes made to DB

--- groundhog.sql
+++ groundhog.sql
@@ -16,31 +16,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `apps`
---
-
-DROP TABLE IF EXISTS `apps`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `apps` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(1000) NOT NULL,
-  `file` varchar(1000) NOT NULL,
-  `args` varchar(1000) DEFAULT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `apps`
---
-
-LOCK TABLES `apps` WRITE;
-/*!40000 ALTER TABLE `apps` DISABLE KEYS */;
-/*!40000 ALTER TABLE `apps` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `caregivers`
 --
 
@@ -54,7 +29,7 @@ CREATE TABLE `caregivers` (
   `patientID` int(11) NOT NULL,
   `password` varchar(1000) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -63,7 +38,7 @@ CREATE TABLE `caregivers` (
 
 LOCK TABLES `caregivers` WRITE;
 /*!40000 ALTER TABLE `caregivers` DISABLE KEYS */;
-INSERT INTO `caregivers` VALUES (1,'andrew','59241',2,'abc123'),(2,'bob','56906',1,'abc123');
+INSERT INTO `caregivers` VALUES (1,'andrew','56125',2,'abc123'),(2,'bob','56906',1,'abc123'),(3,'ben','29430',5,'abc123');
 /*!40000 ALTER TABLE `caregivers` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -81,8 +56,9 @@ CREATE TABLE `events` (
   `allDay` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT 'false',
   `patientID` int(11) DEFAULT NULL,
   `argList` varchar(1000) COLLATE utf8_bin DEFAULT NULL,
+  `className` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=243 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=600 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -91,7 +67,7 @@ CREATE TABLE `events` (
 
 LOCK TABLES `events` WRITE;
 /*!40000 ALTER TABLE `events` DISABLE KEYS */;
-INSERT INTO `events` VALUES (238,'Play Song Playlist','2015-05-02 06:00:00','',2,'_'),(239,'Grooming Reminder','2015-04-29 09:00:00','',2,'_'),(240,'Question and Response','2015-04-27 08:00:00','true',2,'hope_this works great'),(241,'Question and Response','2015-04-30 07:30:00','true',2,'hi_folks'),(242,'Question and Response','2015-04-28 00:00:00','true',2,'sss_mmm');
+INSERT INTO `events` VALUES (385,'Prayer Reminder','2015-05-07 04:30:00','',2,'',''),(389,'Fraud Prevention','2015-05-04 02:00:00','',2,'','fc-event'),(536,'Prayer Reminder','2015-05-03 04:30:00','',2,'','fc-event'),(537,'Medication Reminder','2015-05-03 01:30:00','',2,'','fc-event'),(547,'Question and Response','2015-05-05 00:00:00','true',5,'what time is dinner_dinner is at five thirty','fc-event-question'),(548,'Fraud Prevention','2015-05-04 08:00:00','',5,'','fc-event'),(552,'Pet Feeding Reminder','2015-05-04 15:00:00','',5,'','fc-event'),(553,'Prayer Reminder','2015-05-04 06:30:00','',5,'','fc-event'),(554,'Grooming Reminder','2015-05-04 12:00:00','',5,'','fc-event'),(555,'Oven Reminder','2015-05-04 17:00:00','',5,'','fc-event'),(556,'Question and Response','2015-05-04 00:00:00','true',5,'Is your diaper wet?_Change your diaper!','fc-event-question'),(557,'Question and Response','2015-05-06 00:00:00','true',5,'When can I go out?_Never','fc-event-question'),(578,'Fraud Prevention','2015-05-03 00:00:00','',2,'','fc-event'),(579,'Oven Reminder','2015-05-04 00:00:00','',2,'','fc-event'),(582,'Question and Response','2015-05-03 00:00:00','true',2,'s_a','fc-event-question'),(585,'Grooming Reminder','2015-05-05 10:00:00','',2,'','fc-event'),(586,'Prayer Reminder','2015-05-05 10:00:00','',2,'','fc-event'),(589,'Medication Reminder','2015-05-05 06:30:00','',2,'','fc-event'),(590,'Oven Reminder','2015-05-05 09:00:00','',2,'','fc-event'),(591,'Medication Reminder','2015-05-05 13:00:00','',2,'','fc-event'),(592,'Oven Reminder','2015-05-07 09:30:00','',2,'','fc-event'),(593,'Play Favorite Song','2015-05-05 12:00:00','',2,'','fc-event'),(594,'Play Favorite Song','2015-05-04 13:00:00','',2,'','fc-event'),(595,'Medication Reminder','2015-05-03 10:00:00','',2,'','fc-event'),(596,'Grooming Reminder','2015-05-03 10:30:00','',2,'','fc-event'),(597,'Play Favorite Song','2015-05-03 11:30:00','',2,'','fc-event'),(598,'Play Favorite Song','2015-05-04 11:00:00','',2,'','fc-event'),(599,'Fraud Prevention','2015-05-03 07:30:00','',2,'','fc-event');
 /*!40000 ALTER TABLE `events` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -112,7 +88,7 @@ CREATE TABLE `patients` (
   `cookie` varchar(1000) DEFAULT NULL,
   `fraudNumber` varchar(1000) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -121,58 +97,8 @@ CREATE TABLE `patients` (
 
 LOCK TABLES `patients` WRITE;
 /*!40000 ALTER TABLE `patients` DISABLE KEYS */;
-INSERT INTO `patients` VALUES (1,'Andrew','McConachie','4124184165','cactus','5106466190','44463',NULL),(2,'Andrew','Smutt','smutticus','dragon','sip:smutt@iptel.org','61381','sip:smutt@iptel.org'),(3,'Renu','Bora','3104355344','tiger',NULL,'18782',NULL),(4,'Noriko','Misra','8478685734','puppy',NULL,NULL,NULL);
+INSERT INTO `patients` VALUES (1,'Andrew','McConachie','4124184165','cactus','5106466190','32434',NULL),(2,'Andrew','Smutt','smutticus','dragon','sip:smutt@iptel.org','23912','sip:smutt@iptel.org'),(3,'Renu','Bora','3104355344','tiger',NULL,'18782',NULL),(4,'Noriko','Misra','8478685734','puppy',NULL,NULL,NULL),(5,'Ben','Shapiro','3104300405','tofu','5106466190','62515',NULL);
 /*!40000 ALTER TABLE `patients` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `sched`
---
-
-DROP TABLE IF EXISTS `sched`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `sched` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `timestamp` int(11) NOT NULL,
-  `appID` int(11) NOT NULL,
-  `patientID` int(11) NOT NULL,
-  `args` varchar(1000) DEFAULT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `sched`
---
-
-LOCK TABLES `sched` WRITE;
-/*!40000 ALTER TABLE `sched` DISABLE KEYS */;
-/*!40000 ALTER TABLE `sched` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `songs`
---
-
-DROP TABLE IF EXISTS `songs`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `songs` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(1000) NOT NULL,
-  `file` varchar(1000) NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `songs`
---
-
-LOCK TABLES `songs` WRITE;
-/*!40000 ALTER TABLE `songs` DISABLE KEYS */;
-/*!40000 ALTER TABLE `songs` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -184,4 +110,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-04-26 13:28:22
+-- Dump completed on 2015-05-07 12:14:50
